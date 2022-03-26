eeda88d16586304ffe13c1fca4b816ca35ab9c28
Joshua Kelly <jdkpersonal2015@gmail.com>
2022-03-26 11:51:40 -0400

    ajshggf

--- script.js
+++ script.js
@@ -1,251 +1,247 @@
-const triviaQuestions = [
-  // question one
-  {
-    id: 1,
-    question: "Which geometric shape is used for stop signs?",
-    answers: [
-      { num: 1, answer: "Square", correct: false },
-      { num: 2, answer: "Triangle", correct: false },
-      { num: 3, answer: "Octagon", correct: true },
-      { num: 4, answer: "Hexagon", correct: false },
-    ],
-  },
+// const triviaQuestions = [
+//   // question one
+//   {
+//     id: 1,
+//     question: "Which geometric shape is used for stop signs?",
+//     answers: [
+//       { num: 1, answer: "Square", correct: false },
+//       { num: 2, answer: "Triangle", correct: false },
+//       { num: 3, answer: "Octagon", correct: true },
+//       { num: 4, answer: "Hexagon", correct: false },
+//     ],
+//   },
 
-  // question two
-  {
-    id: 2,
-    question: "What is cynophobia?",
-    answers: [
-      { num: 5, answer: "Fear of Dogs", correct: true },
-      { num: 6, answer: "Fear of Cats", correct: false },
-      { num: 7, answer: "Fear of Dentists", correct: false },
-      { num: 8, answer: "Fear of Doctors", correct: false },
-    ],
-  },
-  // question three
-  {
-    id: 3,
-    question: "Who named the Pacific Ocean?",
-    answers: [
-      { num: 9, answer: "Ferdinand the Bull", correct: false },
-      { num: 10, answer: "Ferdinand Magellan", correct: true },
-      { num: 11, answer: "Franz Ferdinand", correct: false },
-      { num: 12, answer: "Ferdinand Marcos", correct: false },
-    ],
-  },
-  // question four
-  {
-    id: 4,
-    question: "What is the biggest tech company in South Korea?",
-    answers: [
-      { num: 13, answer: "Lenovo", correct: false },
-      { num: 14, answer: "Huawei", correct: false },
-      { num: 15, answer: "Mitsibushi", correct: false },
-      { num: 16, answer: "Samsung", correct: true },
-    ],
-  },
-];
+//   // question two
+//   {
+//     id: 2,
+//     question: "What is cynophobia?",
+//     answers: [
+//       { num: 5, answer: "Fear of Dogs", correct: true },
+//       { num: 6, answer: "Fear of Cats", correct: false },
+//       { num: 7, answer: "Fear of Dentists", correct: false },
+//       { num: 8, answer: "Fear of Doctors", correct: false },
+//     ],
+//   },
+//   // question three
+//   {
+//     id: 3,
+//     question: "Who named the Pacific Ocean?",
+//     answers: [
+//       { num: 9, answer: "Ferdinand the Bull", correct: false },
+//       { num: 10, answer: "Ferdinand Magellan", correct: true },
+//       { num: 11, answer: "Franz Ferdinand", correct: false },
+//       { num: 12, answer: "Ferdinand Marcos", correct: false },
+//     ],
+//   },
+//   // question four
+//   {
+//     id: 4,
+//     question: "What is the biggest tech company in South Korea?",
+//     answers: [
+//       { num: 13, answer: "Lenovo", correct: false },
+//       { num: 14, answer: "Huawei", correct: false },
+//       { num: 15, answer: "Mitsibushi", correct: false },
+//       { num: 16, answer: "Samsung", correct: true },
+//     ],
+//   },
+// ];
 
-const mainEl = document.getElementById("main");
+// const mainEl = document.getElementById("main");
 
-class Game {
-  constructor() {
-    this.correctScore = 0;
-    this.incorrectScore = 0;
-    this.unansweredScore = 0;
-    this.time = 3;
-    this.intervalID;
-    this.timeoutID;
-  }
+// class Game {
+//   constructor() {
+//     this.correctScore = 0;
+//     this.incorrectScore = 0;
+//     this.unansweredScore = 0;
+//     this.time = 3;
+//     this.intervalID;
+//     this.timeoutID;
+//   }
 
-  incrementCorrectScore() {
-    return this.correctScore++;
-  }
-  incrementIncorrectScore() {
-    return this.incorrectScore++;
-  }
-  incrementUnansweredScore() {
-    return this.unansweredScore++;
-  }
+//   incrementCorrectScore() {
+//     return this.correctScore++;
+//   }
+//   incrementIncorrectScore() {
+//     return this.incorrectScore++;
+//   }
+//   incrementUnansweredScore() {
+//     return this.unansweredScore++;
+//   }
 
-  resetTime() {
-    this.time = 3;
-  }
+//   resetTime() {
+//     this.time = 3;
+//   }
 
-  startTimer() {
-    document.getElementById("timer").innerText = this.time;
-    this.intervalID = setInterval(() => {
-      this.time--;
-      document.getElementById("timer").innerText = this.time;
-    }, 1000);
+//   startTimer() {
+//     document.getElementById("timer").innerText = this.time;
+//     this.intervalID = setInterval(() => {
+//       this.time--;
+//       document.getElementById("timer").innerText = this.time;
+//     }, 1000);
 
-    this.timeoutID = setTimeout(() => {
-      // Stop the timer
-      this.stopTimer();
+//     this.timeoutID = setTimeout(() => {
+//       // Stop the timer
+//       this.stopTimer();
 
-      // evaluate score
-      this.evalScore();
+//       // evaluate score
+//       this.evalScore();
 
-      // remove quiz
-      document.getElementById("quiz").remove();
+//       // remove quiz
+//       document.getElementById("quiz").remove();
 
-      // Show Game Over Screen
-      this.renderGameOver();
-    }, 1000 * this.time);
-  }
+//       // Show Game Over Screen
+//       this.renderGameOver();
+//     }, 1000 * this.time);
+//   }
 
-  stopTimer() {
-    clearInterval(this.intervalID);
-    clearTimeout(this.timeoutID);
-  }
-  renderGame() {
-    mainEl.innerHTML = `
-    <form>
-        <div id="game">
-         <h1> Trivia Game! </h1>
-          <p> You have a limited amount of time to select your answers. </p>
-          <p> Select your answer by clicking on the radio button corresponding with your answer. </p>
-          <p> Points are added up after the quiz is over, and ONLY if the quiz is finished in time. </p>
-         <h2> Timer:  <span id="timer">--</span> seconds remaining</h2>
-        </div>
+//   renderGame() {
+//     mainEl.innerHTML = `
+//     <form>
+//         <div id="game">
+//          <h1> Trivia Game! </h1>
+//           <p> You have a limited amount of time to select your answers. </p>
+//           <p> Select your answer by clicking on the radio button corresponding with your answer. </p>
+//           <p> Points are added up after the quiz is over, and ONLY if the quiz is finished in time. </p>
+//          <h2> Timer:  <span id="timer">--</span> seconds remaining</h2>
+//         </div>
 
-        <section id="quiz"></section>
-    
-    </form>
-    
-    <button id="start"> Start </button>
-    `;
-    document.querySelector("#start").addEventListener("click", startGame);
-  }
-  renderGameOver() {
-    mainEl.innerHTML = `
-    <div id="game">
-      <h1> GAME OVER </h1>
-      <h3> Correct Answers: <span id="correct">${this.correctScore}</span> </h3>
-      <h3> Incorrect Answers: <span id="incorrect">${this.incorrectScore}</span> </h3>
-      <h3> Unanswered Answers: <span id="unanswered">${this.unansweredScore}</span> </h3>
-      <button id = "quit"> Quit </button> 
-    </div>
-    `;
-    document.getElementById("quit").addEventListener("click", () => {
-      new Game();
-      this.renderGame();
-    });
-  }
+//         <section id="quiz"></section>
 
-  evalScore() {
-    document.querySelectorAll(".answer").forEach((answer) => {
-      if (answer.checked && answer.value === "true") {
-        this.incrementCorrectScore();
-      }
-      if (answer.checked && answer.value === "false") {
-        this.incrementIncorrectScore();
-      }
-    });
-  }
-}
+//     </form>
 
-class Question extends Game {
-  constructor(id, question, answerA, answerB, answerC, answerD) {
-    super();
-    this.id = id;
-    this.question = question;
-    this.answerA = answerA;
-    this.answerB = answerB;
-    this.answerC = answerC;
-    this.answerD = answerD;
-  }
+//     <button id="start"> Start </button>
+//     `;
+//     document.querySelector("#start").addEventListener("click", startGame);
+//   }
+//   renderGameOver() {
+//     mainEl.innerHTML = `
+//     <div id="game">
+//       <h1> GAME OVER </h1>
+//       <h3> Correct Answers: <span id="correct">${this.correctScore}</span> </h3>
+//       <h3> Incorrect Answers: <span id="incorrect">${this.incorrectScore}</span> </h3>
+//       <h3> Unanswered Answers: <span id="unanswered">${this.unansweredScore}</span> </h3>
+//       <button id = "quit"> Quit </button>
+//     </div>
+//     `;
+//     document.getElementById("quit").addEventListener("click", () => {
+//       new Game();
+//       this.renderGame();
+//     });
+//   }
 
-  renderQuiz() {
-    return (document.getElementById("quiz").innerHTML += `
-        <fieldset id="question${this.id}">
-             <legend> <h3>${this.question}</h3> </legend>
-            <ul>
-                <li>
-                    <input type="radio" id="${this.answerA.num}" class="answer" name="${this.id}" value="${this.answerA.correct}">
-                    <label for="${this.answerA.num}">${this.answerA.answer}</label>
-                </li>
+//   evalScore() {
+//     document.querySelectorAll(".answer").forEach((answer) => {
+//       if (answer.checked && answer.value === "true") {
+//         this.incrementCorrectScore();
+//       }
+//       if (answer.checked && answer.value === "false") {
+//         this.incrementIncorrectScore();
+//       }
+//     });
+//   }
+// }
 
-                <li>
-                    <input type="radio" id="${this.answerB.num}" class="answer" name="${this.id}" value="${this.answerB.correct}">
-                    <label for="${this.answerB.num}">${this.answerB.answer}</label>
-                </li>
+// class Question extends Game {
+//   constructor(id, question, answerA, answerB, answerC, answerD) {
+//     super();
+//     this.id = id;
+//     this.question = question;
+//     this.answerA = answerA;
+//     this.answerB = answerB;
+//     this.answerC = answerC;
+//     this.answerD = answerD;
+//   }
 
-                <li>
-                    <input type="radio" id="${this.answerC.num}" class="answer" name="${this.id}" value="${this.answerC.correct}">
-                    <label for="${this.answerC.num}">${this.answerC.answer}</label>
-                </li>
+//   renderQuiz() {
+//     return (document.getElementById("quiz").innerHTML += `
+//         <fieldset id="question${this.id}">
+//              <legend> <h3>${this.question}</h3> </legend>
+//             <ul>
+//                 <li>
+//                     <input type="radio" id="${this.answerA.num}" class="answer" name="${this.id}" value="${this.answerA.correct}">
+//                     <label for="${this.answerA.num}">${this.answerA.answer}</label>
+//                 </li>
 
-                <li>
-                    <input type="radio" id="${this.answerD.num}" class="answer" name="${this.id}" value="${this.answerD.correct}">
-                    <label for="${this.answerD.num}">${this.answerD.answer}</label>
-                </li>
-            </ul>
-        </fieldset>    
-    `);
-  }
-}
+//                 <li>
+//                     <input type="radio" id="${this.answerB.num}" class="answer" name="${this.id}" value="${this.answerB.correct}">
+//                     <label for="${this.answerB.num}">${this.answerB.answer}</label>
+//                 </li>
 
-function submitQuiz(e) {
-  // prevent default so i can manipulate data
-  e.preventDefault();
+//                 <li>
+//                     <input type="radio" id="${this.answerC.num}" class="answer" name="${this.id}" value="${this.answerC.correct}">
+//                     <label for="${this.answerC.num}">${this.answerC.answer}</label>
+//                 </li>
 
-  // Stop the timer when user finish quiz
-  game.stopTimer();
+//                 <li>
+//                     <input type="radio" id="${this.answerD.num}" class="answer" name="${this.id}" value="${this.answerD.correct}">
+//                     <label for="${this.answerD.num}">${this.answerD.answer}</label>
+//                 </li>
+//             </ul>
+//         </fieldset>
+//     `);
+//   }
+// }
 
-  // evaluate score
-  game.evalScore();
+// function submitQuiz(e) {
+//   // prevent default so i can manipulate data
+//   e.preventDefault();
 
-  // remove quiz
-  document.getElementById("quiz").remove();
+//   // Stop the timer when user finish quiz
+//   game.stopTimer();
 
-  // Show Game Over Screen
-  game.renderGameOver();
-}
+//   // evaluate score
+//   game.evalScore();
 
-function createQuiz() {
-  // Loop through array of all my questions and
-  triviaQuestions.forEach((q) => {
-    // destructuring really comes in handy lol
-    const { id, question, answers } = q;
-    const [answerA, answerB, answerC, answerD] = answers;
+//   // remove quiz
+//   document.getElementById("quiz").remove();
 
-    // Create a new question object for each one
-    const quiz = new Question(id, question, answerA, answerB, answerC, answerD);
+//   // Show Game Over Screen
+//   game.renderGameOver();
+// }
 
-    // render quiz to screen
-    quiz.renderQuiz();
-  });
+// function createQuiz() {
+//   // Loop through array of all my questions and
+//   triviaQuestions.forEach((q) => {
+//     // destructuring really comes in handy lol
+//     const { id, question, answers } = q;
+//     const [answerA, answerB, answerC, answerD] = answers;
 
-  // create submit button, append to quiz element
-  const submitBtn = document.createElement("button");
-  submitBtn.innerText = "Submit Quiz";
-  submitBtn.id = "submit";
-  submitBtn.addEventListener("click", submitQuiz);
-  document.getElementById("quiz").appendChild(submitBtn);
+//     // Create a new question object for each one
+//     const quiz = new Question(id, question, answerA, answerB, answerC, answerD);
 
-  // remove button
-  document.getElementById("start").remove();
-}
+//     // render quiz to screen
+//     quiz.renderQuiz();
+//   });
 
-function startGame() {
-  if (game.time != 10) {
-    game.time = 10;
-  }
-  if (game.correctScore || game.incorrectScore != 0) {
-    game.correctScore = 0;
-    game.incorrectScore = 0;
-  }
-  // first create the quiz
-  createQuiz();
+//   // create submit button, append to quiz element
+//   const submitBtn = document.createElement("button");
+//   submitBtn.innerText = "Submit Quiz";
+//   submitBtn.id = "submit";
+//   submitBtn.addEventListener("click", submitQuiz);
+//   document.getElementById("quiz").appendChild(submitBtn);
 
-  // start timer
-  game.startTimer();
-}
+//   // remove button
+//   document.getElementById("start").remove();
+// }
 
-// Declare a new Game automatically.. i guess.
-const game = new Game();
+// function startGame() {
+//   if (game.time != 10) {
+//     game.time = 10;
+//   }
+//   if (game.correctScore || game.incorrectScore != 0) {
+//     game.correctScore = 0;
+//     game.incorrectScore = 0;
+//   }
+//   // first create the quiz
+//   createQuiz();
 
-game.renderGame();
+//   // start timer
+//   game.startTimer();
+// }
 
-document.getElementById("start").addEventListener("click", startGame);
+// // Declare a new Game automatically.. i guess.
+// const game = new Game();
+
+// game.renderGame();
+
+// document.getElementById("start").addEventListener("click", startGame);
